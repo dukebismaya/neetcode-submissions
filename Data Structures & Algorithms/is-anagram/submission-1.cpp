2971899d1db0a0c1e70841a55d4b87f9398a3b9d
BISMAYA JYOTI DALEI <179898597+dukebismaya@users.noreply.github.com>
2026-06-26 13:34:39 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // Sort both string and compare O(n log n)
+        if (s.length()!=t.length()) return false;
+        std::sort(s.begin(), s.end());
+        std::sort(t.begin(), t.end());
+        return s==t;
+        
+    }
+};
