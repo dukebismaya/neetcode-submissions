5bf800b1295aeffd741a71fe880e6afcbfaf29a4
BISMAYA JYOTI DALEI <179898597+dukebismaya@users.noreply.github.com>
2026-06-29 18:41:11 +0530

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int longest_streak{};
+        std::unordered_set<int>num_set(nums.begin(), nums.end());
+        for (const auto num: nums){
+            if (num_set.find(num-1)==num_set.end()){
+                int curr_el = num;
+                int curr_streak = 1;
+                while (num_set.find(curr_el+1)!=num_set.end()){
+                    curr_el+=1;
+                    curr_streak++;
+                }
+                longest_streak = std::max(curr_streak, longest_streak);
+            }
+        }
+        return longest_streak;
+    }
+};
