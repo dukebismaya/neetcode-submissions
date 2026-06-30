74a7cdf90ec2a13f8ea545fa3e5f88d547f0003e
BISMAYA JYOTI DALEI <179898597+dukebismaya@users.noreply.github.com>
2026-06-30 20:31:11 +0530

    Add: three-integer-sum - submission-7

---
+++ Data Structures & Algorithms/three-integer-sum/submission-7.cpp
@@ -0,0 +1,25 @@
+class Solution {
+   public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::sort(nums.begin(), nums.end());
+        std::vector<std::vector<int>> res;
+        int num_len = nums.size();
+        for (int i = 0; i < num_len; ++i) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            std::unordered_set<int> seen;
+            for (int j = i + 1; j < num_len; ++j) {
+                int complement = -(nums[i] + nums[j]);
+                if (seen.count(complement)) {
+                    res.push_back({nums[i], nums[j], complement});
+                    while (j + 1 < num_len && nums[j] == nums[j + 1]) ++j;
+                }
+                seen.insert(nums[j]);
+            }
+        }
+
+        return res;
+    }
+};
